917026b2b196ef57b4b4b5bee93dc33a4ca2ee48
Krzysztof Narloch <knarloch@gmail.com>
2024-02-25 23:53:20 +0100

    Update instal_deps.sh to be non-interactive

--- install_deps.sh
+++ install_deps.sh
@@ -4,6 +4,6 @@ set -ex
 
 sudo apt install gcc-aarch64-linux-gnu
 
-rustup -V || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup -V || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 rustup target add aarch64-unknown-linux-gnu
