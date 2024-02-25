a94bf694978d35dd84c9f31c79558a6084a359c9
Krzysztof Narloch <knarloch@gmail.com>
2024-02-26 00:01:25 +0100

    Do not require shell restart after installing rustup

--- install_deps.sh
+++ install_deps.sh
@@ -5,5 +5,6 @@ set -ex
 sudo apt install gcc-aarch64-linux-gnu
 
 rustup -V || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
 
 rustup target add aarch64-unknown-linux-gnu
